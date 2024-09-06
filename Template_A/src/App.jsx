574675f254d2ece5209a3657401a246fec40c7a5
Aneesh165 <aneeshmp165@gmail.com>
2024-09-06 13:33:33 +0530

    6-8-24

--- Template_A/src/App.jsx
+++ Template_A/src/App.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
-import './App.css'
-import Product from './Components/Product'
-import Navbarss from './Components/Navbar'
+import React from "react";
+import "./App.css";
+import Product from "./Components/Product";
+import Navbarss from "./Components/Navbar";
 
 function App() {
-  return (<>
-    <Navbarss />
-    <div className='main'>
-      
-      <h1 style={{textAlign:'center'}}>PRODUCTS</h1>
-      <Product/>
-    </div>
+  const Data = [
+    {
+      name: "Shoe 1",
+      image:
+        "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSZvkZxHa1P3b6MUQUi6y04swub6O01-FFH-4rW0zvS94bTXm-kK6W27DWsetoO-uDZm0k3MvF1GvqkDZg6Z8mP9hn8WN9VytFd-tPjlSKSQqrSA6-fNzDvLg",
+      desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      price: 50,
+    },
+    {
+      name: "Shoe 2",
+      image:
+        "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTu2pMlKRLp-RgcfkONvhnUzmWwRmGI_ebhKhdePkvki-sG6rirVraYGAiEgw5ZNLnz1FVl1Llt7LCTAIjxxI4fK1G3uHMsrkJbbdBl0d29HcMn88cHTsq5OQ",
+      desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      price: 40,
+    },
+    {
+      name: "Shoe 3",
+      image:
+        "https://rukminim2.flixcart.com/image/600/600/xif0q/shoe/3/z/t/-original-imagx9kwqhyqsgqz.jpeg",
+      desc: " Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+      price: 30,
+    },
+  ];
+  return (
+    <>
+      <Navbarss />
+      <div className="main">
+        <h1 style={{ textAlign: "center" }}>PRODUCTS</h1>
+
+        <div className="flex sm:flex-row flex-col justify-center items-center w-[80%] mx-auto my-6 border p-4 gap-4 shadow-md">
+          {Data.map((item, index) => {
+            return (
+              <Product
+                name={item.name}
+                image={item.image}
+                price={item.price}
+                desc={item.desc}
+              />
+            );
+          })}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Template_A/src/Components/Product.jsx
+++ Template_A/src/Components/Product.jsx
@@ -1,40 +1,16 @@
-import React from 'react'
+import React from "react";
 
-function Product() {
+function Product({ name, price, image ,desc}) {
   return (
-    <div className='template_body'>
-    <div className='template_item'>
-        <div className='image'>
-          <img src="https://rukminim2.flixcart.com/image/600/600/xif0q/shoe/3/z/t/-original-imagx9kwqhyqsgqz.jpeg" alt="Nike Shoe" />
-        </div>
-        <div className='detail'>
-          <h2>NIKE</h2>
-          <p>Best quality shoes that can last longer.</p>
-          <h3>$120</h3>
-        </div>
+    <div className="flex flex-col  justify-center items-center text-center shadow-lg">
+      <div className="p-2 ">
+        <img className="w-[500px] h-[300px] object-cover" src={image} />
       </div>
-    <div className='template_item'>
-        <div className='image'>
-          <img src="https://assets.ajio.com/medias/sys_master/root/20230415/ZU8n/6439f0b0907deb497ae7a4d7/-600Wx600H-466060513-black-MODEL.jpg" alt="Nike Shoe" />
-        </div>
-        <div className='detail'>
-          <h2>PUMA</h2>
-          <p>Best quality shoes that gives comforts.</p>
-          <h3>$99</h3>
-        </div>
-      </div>
-    <div className='template_item'>
-        <div className='image'>
-          <img src="https://www.dashingboot.com/cdn/shop/products/download-2022-06-09T123436.538.png" alt="Nike Shoe" />
-        </div>
-        <div className='detail'>
-          <h2>NEW BALANCE</h2>
-          <p>Best quality shoes that are expensive</p>
-          <h3>$120</h3>
-        </div>
-      </div>
-      </div>
-  )
+      <div>{name}</div>
+      <div>{price}</div>
+      <div><p>{desc}</p></div>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
