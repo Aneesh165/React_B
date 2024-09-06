78c30ce35b815d7cde5e0b1ca8d4bfd8d33484a6
Aneesh165 <aneeshmp165@gmail.com>
2024-09-06 11:25:36 +0530

    06-8-24

--- Template_A/src/App.jsx
+++ Template_A/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import Product from './Components/Product'
+import Navbarss from './Components/Navbar'
 
 function App() {
   return (
     <div className='main'>
+      <Navbarss/>
       <h1 style={{textAlign:'center'}}>PRODUCTS</h1>
       <Product/>
     </div>
