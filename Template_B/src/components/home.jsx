ac928e22a6f38a0f0b084231a702a50a92a7cea8
Aneesh165 <aneeshmp165@gmail.com>
2024-09-07 13:01:09 +0530

    7-8-24

---
+++ Template_B/src/components/home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="w-[80%] mt-9 mx-auto flex justify-between">
+      <div className="w-[50%]">
+        <h1 className="text-8xl">
+          YOUR FEET <br /> DESERVE <br /> THE BEST
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit. Saepe,
+          possimus! Porro <br /> earum corrupti recusandae ab quibusdam <br />{" "}
+          dolores voluptatem, aliquid .
+        </p>
+        <div className="container-fluid flex-row justify-center ">
+          <button variant="danger">Danger</button>
+
+          <button variant="secondary">Secondary</button>
+        </div>
+      </div>
+      <div className="w-[50%">hdbdvb
+        <img src="" />
+      </div>
+    </div>
+  )
+}
+
+export default Home
