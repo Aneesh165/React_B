038cc497df6b6d53b9ab80bd90b7b6dcb0229b83
Aneesh165 <aneeshmp165@gmail.com>
2024-09-08 14:26:34 +0530

    8-9-24

--- Project_1/src/App.jsx
+++ Project_1/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    // Apply dark mode class to the HTML element
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
   return (
-    <div></div>
-  )
+    <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white">
+      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+    </div>
+  );
 }
 
-export default App
+export default App;
