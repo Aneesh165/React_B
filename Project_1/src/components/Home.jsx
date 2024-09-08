483ae1442465fbb2369613ebfee5a12070c9d448
Aneesh165 <aneeshmp165@gmail.com>
2024-09-08 18:07:56 +0530

    8-9-24

--- Project_1/src/components/Home.jsx
+++ Project_1/src/components/Home.jsx
@@ -4,26 +4,26 @@ function Home() {
   const [modalExpanded, setModalExpanded] = useState(false);
 
   return (<>
-     <div className='w-[80%] h-[680px] mx-auto flex justify-between '>
-        <div className='w-[60%]'></div>
-        <div className="relative w-[40%]  flex justify-end ">
-        <div className="relative w-[80%]">
-  {/* Mockup image inside the phone */}
-  <img
-    className="absolute inset-0 right-5 w-[86%] h-full object-contain rounded-[60px]"
-    src="https://preview.cruip.com/appy/images/mockup-image-01.jpg"
-    alt="Features illustration"
-  />
-  
-  {/* iPhone mockup image */}
-  <img
-    className="relative z-10 h-[680px] rounded-[60px] overflow-hidden"
-    src="https://preview.cruip.com/appy/images/iphone-mockup.png"
-    alt="iPhone mockup"
-  />
+     <div className='w-[80%] h-[690px] mx-auto flex justify-between '>
+        <div className='w-[60%] h-[680px]'>f</div>
+        <div className="relative w-[40%] h-[680px] flex justify-end overflow-hidden">
+  <div className="relative w-[80%]">
+    {/* Mockup image inside the phone */}
+    <img
+      className="absolute top-0 left-4 w-[83%] h-full object-cover rounded-[70px] "
+      src="https://preview.cruip.com/appy/images/mockup-image-01.jpg"
+      alt="Features illustration"
+    />
+    
+    {/* iPhone mockup image */}
+    <img
+      className="relative z-10 h-full"
+      src="https://preview.cruip.com/appy/images/iphone-mockup.png"
+      alt="iPhone mockup"
+    />
+  </div>
 </div>
 
-        </div>
      </div>
   </>
  
