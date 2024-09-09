acd9c3680615aff80169d4249cae01051bd15ef6
Aneesh165 <aneeshmp165@gmail.com>
2024-09-09 11:32:36 +0530

    9-9-24

--- Project_1/src/App.jsx
+++ Project_1/src/App.jsx
@@ -1,23 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Carousel from "./components/Carousel";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
-    // Apply dark mode class to the HTML element
     if (darkMode) {
-      document.documentElement.classList.add('dark');
+      document.documentElement.classList.add("dark");
     } else {
-      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.remove("dark");
     }
   }, [darkMode]);
 
-  return (//bg-gradient-to-bl from-sky-200 to-white dark:bg-gradient-to-bl dark:from-gray-950 dark:to-gray-800
+  return (
+    //bg-gradient-to-bl from-sky-200 to-white dark:bg-gradient-to-bl dark:from-gray-950 dark:to-gray-800
     <div className="w-full h-[600vh] bg-white dark:bg-gray-900   text-black dark:text-white">
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-      <Home/>
+      <Home />
+      <Carousel />
     </div>
   );
 }
---
+++ Project_1/src/components/Carousel.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Carousel() {
+  return (
+    <div className="flex">
+      <div className="flex flex-col text-center mx-auto ">
+        <h1 className="text-6xl font-bold">
+          From rough design files, to <br /> powerful products
+        </h1>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat <br /> nulla pariatur excepteur sint occaecat
+          cupidatat.
+        </p>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Carousel;
