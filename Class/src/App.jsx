e72e9e2361256470492ace5776eb15d1087397f9
Aneesh165 <aneeshmp165@gmail.com>
2024-09-10 21:10:09 +0530

    10-9-24

--- Class/src/App.jsx
+++ Class/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import Classes from './component/Classes'
-
+import Routerdom from './component/Routerdom'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import About from './component/About'
+import Contact from './component/Contact'
+import Services from './component/Services'
 function App() {
   return (
     <div>
-      <Classes/>
+
+<BrowserRouter>
+<Routerdom/>
+<Routes>
+<Route path='/About' element={<About/>}></Route>
+<Route path='/Contact' element={<Contact/>}></Route>
+<Route path='/Services' element={<Services/>}></Route>
+</Routes>
+</BrowserRouter>
     </div>
   )
 }
