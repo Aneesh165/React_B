680a287203f6d0101ba68add7f1322511da7c0e3
Aneesh165 <aneeshmp165@gmail.com>
2024-09-14 21:57:43 +0530

    14-9-24

--- Project_1/src/components/Reality.jsx
+++ Project_1/src/components/Reality.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Reality() {
   return (
-    <div className=''>
-        <h1>Turn your ideas into reality in <br /> seconds</h1>
+    <div className='text-center'>
+        <h1 className=''>Turn your ideas into reality in <br /> seconds</h1>
         <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat <br /> nulla pariatur excepteur sint occaecat cupidatat.</p>
     </div>
   )
