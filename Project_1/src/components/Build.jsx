7078894da6d3789754f7c1dbaf8520d47cfc1326
Aneesh165 <aneeshmp165@gmail.com>
2024-09-15 16:09:15 +0530

    15-9-24

--- Project_1/src/components/Build.jsx
+++ Project_1/src/components/Build.jsx
@@ -4,8 +4,8 @@ function Build() {
   return (
     <div className='text-center   dark:bg-[#1f1f22]'>
         <div>
-            <h1>Build and style every element to perfection</h1>
-            
+            <h1>Build and style every <br /> element to perfection</h1>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat <br /> nulla pariatur excepteur sint occaecat cupidatat.</p>
         </div>
     </div>
   )
