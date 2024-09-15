65d55dbc398d630f64c839178713293e7441e476
Aneesh165 <aneeshmp165@gmail.com>
2024-09-15 16:08:29 +0530

    15-9-24

--- Project_1/src/components/Build.jsx
+++ Project_1/src/components/Build.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function Build() {
   return (
     <div className='text-center   dark:bg-[#1f1f22]'>
-        
+        <div>
+            <h1>Build and style every element to perfection</h1>
+            
+        </div>
     </div>
   )
 }
