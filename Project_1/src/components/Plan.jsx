122331fd60525720067465cd449af313172dd92d
Aneesh165 <aneeshmp165@gmail.com>
2024-09-16 12:26:16 +0530

    16-9-24

--- Project_1/src/components/Plan.jsx
+++ Project_1/src/components/Plan.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Plan() {
   return (
-    <div className="bg-[#1d1d20] text-white py-6">
-      <div className=" text-center mx-auto pt-10">
+    <div className="bg-[#212124] text-white py-6">
+      <div className=" text-center mx-auto pt-10 pb-8">
         <h1 className="text-6xl font-bold py-2">
           Start building for free, then <br /> add a plan to go live
         </h1>
@@ -12,7 +12,7 @@ function Plan() {
           officia deserunt <br /> mollit laborum — semper quis lectus nulla.
         </p>
       </div>
-      <div className="w-[80%] mx-auto flex justify-between gap-5 px-4">
+      <div className="w-[80%] mx-auto flex justify-between gap-5 px-4 pb-20">
         <div className="w-[23%]">
           <h4 className="text-xl font-semibold py-4">All plans include:</h4>
           <p className="flex text-md text-gray-400 py-1 gap-1">
@@ -104,46 +104,69 @@ function Plan() {
             Smart forms and reports
           </p>
         </div>
-        <div className="w-[23%] bg-white text-black flex flex-col justify-between">
+        <div className="w-[23%] bg-white text-black flex flex-col justify-between dark:text-white dark:bg-[#2e2e33] border-2 border-[#2e2e33] dark:hover:border-[#3abab4]">
           <div className="w-[75%] mx-auto mb-[138px]">
             <div className="flex justify-between my-2">
               <h5 className="text-2xl font-semibold py-1">Yearly</h5>
-              <p className="my-2  bg-teal-200 text-teal-600 rounded-2xl px-2">-40%</p>
+              <p className="my-2  bg-teal-200 text-teal-600 rounded-2xl px-2">
+                -40%
+              </p>
             </div>
             <div className="flex my-2">
-            <p className="text-gray-700 text-2xl font-bold mt-auto">$</p>
-            <h1 className="text-5xl font-bold">27</h1>
-            <p className="text-gray-700 text-lg  mt-auto">/billedyearly</p>
+              <p className="text-gray-500 text-2xl font-bold mt-auto">$</p>
+              <h1 className="text-5xl font-bold">27</h1>
+              <p className="text-gray-500 text-lg  mt-auto">/billedyearly</p>
             </div>
             <div className="text-gray-500">
               <p>— Lorem ipsum dolor amet sit consect adipiscing.</p>
-              </div>
+            </div>
           </div>
           <div className="mx-auto mb-6">
-            <button className="px-14 py-2 text-white font-semibold bg-[#3abab4]">GO Premium</button>
+            <button className="px-14 py-2 text-white font-semibold bg-[#9f7aea] hover:bg-[#3abab4]">
+              GO Premium
+            </button>
           </div>
         </div>
-        <div className="w-[23%] bg-white text-black">
-          <div className="w-[75%] mx-auto">
-            <div className="flex justify-between">
-              <h5>Yearly</h5>
-              <p>-40%</p>
+        <div className="w-[23%] bg-white text-black flex flex-col justify-between dark:text-white dark:bg-[#2e2e33] border-2 border-[#2e2e33] dark:hover:border-[#3abab4]">
+          <div className="w-[75%] mx-auto mb-[138px]">
+            <div className="flex justify-between my-2">
+              <h5 className="text-2xl font-semibold py-1">Monthly</h5>
+              
             </div>
-            <div>$27</div>
-            <div>— Lorem ipsum dolor amet sit consect adipiscing.</div>
+            <div className="flex my-2">
+              <p className="text-gray-500 text-2xl font-bold mt-auto">$</p>
+              <h1 className="text-5xl font-bold">47</h1>
+              <p className="text-gray-500 text-lg  mt-auto">/billedyearly</p>
+            </div>
+            <div className="text-gray-500">
+              <p>— Lorem ipsum dolor amet sit consect adipiscing.</p>
+            </div>
           </div>
-          <div></div>
+          <div className="mx-auto mb-6">
+            <button className="px-14 py-2 text-white font-semibold bg-[#9f7aea] hover:bg-[#3abab4]">
+              GO Premium
+            </button>
+          </div>
         </div>
-        <div className="w-[23%] bg-white text-black">
-          <div className="w-[75%] mx-auto ">
-            <div className="flex justify-between">
-              <h5>Yearly</h5>
-              <p>-40%</p>
+        <div className="w-[23%] bg-white text-black flex flex-col justify-between dark:text-white dark:bg-[#2e2e33] border-2 border-[#2e2e33] dark:hover:border-[#3abab4]">
+          <div className="w-[75%] mx-auto mb-[138px]">
+            <div className="flex justify-between my-2">
+              <h5 className="text-2xl font-semibold py-1">Lifetime</h5>
             </div>
-            <div>$27</div>
-            <div>— Lorem ipsum dolor amet sit consect adipiscing.</div>
+            <div className="flex my-2">
+              <p className="text-gray-500 text-2xl font-bold mt-auto">$</p>
+              <h1 className="text-5xl font-bold">127</h1>
+              <p className="text-gray-500 text-lg  mt-auto">/one-time</p>
+            </div>
+            <div className="text-gray-500">
+              <p>— Lorem ipsum dolor amet sit consect adipiscing.</p>
+            </div>
           </div>
-          <div></div>
+          <div className="mx-auto mb-6">
+            <button className="px-14 py-2 text-white font-semibold bg-[#9f7aea] hover:bg-[#3abab4]">
+              GO Premium
+            </button>
+          </div>
         </div>
       </div>
     </div>
