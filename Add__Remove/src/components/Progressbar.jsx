16bc2e0b402c62c32660ff0e52cf20bb668d5fb7
Aneesh165 <aneeshmp165@gmail.com>
2024-09-20 23:12:17 +0530

    20-9-24

--- Add__Remove/src/components/Progressbar.jsx
+++ Add__Remove/src/components/Progressbar.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Progress } from "@material-tailwind/react";
 
 function Progressbar() {
-    const now = 60;
+  const [progress, setprogress] = useState(0);
+
+  const updateprogress = (now) => {
+    const { left, width } = now.target.getBoundingClientRect();
+    let newProgress = ((now.clientX - left) / width) * 100;
+
+    setprogress(Math.round(newProgress)); 
+  };
+
   return (
-    <div>
-        
-    </div>
-  )
+    <>
+      <div className="w-[50%] mx-auto mt-20" onClick={updateprogress}>
+        <h1>Progress :{progress}%</h1>
+        <Progress value={progress} size="lg" color="blue"/>
+      </div>
+    </>
+  );
 }
 
-export default Progressbar
+export default Progressbar;
