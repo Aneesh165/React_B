bc44ed0b5c5860eaa96c35501e9ee8ca19e3af85
Aneesh165 <aneeshmp165@gmail.com>
2024-10-05 11:47:46 +0530

    5-9-24

---
+++ missingnum.js
@@ -0,0 +1,27 @@
+function missingNumber(){
+
+let number=[3,1,6,8];
+let Max = Math.max(...number)
+let Num=1
+
+let output=[]
+
+for(i=Num;i<=Max;i++){
+   if(!number.includes(i)){
+    output.push(i)
+   }
+
+}
+console.log(output);
+
+
+
+
+
+
+
+
+
+}
+
+missingNumber()
