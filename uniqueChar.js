3ffd610b4f6a415e0e2e16d1fd4a2a875f2439a5
Aneesh165 <aneeshmp165@gmail.com>
2024-10-06 17:47:37 +0530

    6-9-24

--- uniqueChar.js
+++ uniqueChar.js
@@ -1,1 +1,30 @@
-let char=''
+let string = "cabab";
+
+let counts = {};
+let positions = {};
+let nrpositions = [];
+
+
+for (let i = 0; i < string.length; i++) {
+  let char = string[i];
+  counts[char] = (counts[char] || 0) + 1;
+
+  if (!positions[char]) {
+    positions[char] = [];
+  }
+  positions[char].push(i);
+  
+}
+
+// console.log(counts);
+
+// console.log(positions);
+
+
+for (let char in counts) {
+  if (counts[char] === 1) {
+    nrpositions.push(...positions[char]);
+  }
+}
+
+nrpositions.length > 0 ? console.log(nrpositions) : console.log("-1");
