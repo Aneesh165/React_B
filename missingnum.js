741e7ca2b1f883c80f4d8e858ff82384a072e172
Aneesh165 <aneeshmp165@gmail.com>
2024-10-07 11:24:41 +0530

    7-9-24

--- missingnum.js
+++ missingnum.js
@@ -1,27 +1,16 @@
-function missingNumber(){
+function missingNumber() {
+  let number = [3, 1, 6, 8];
+  let Max = Math.max(...number);
+  let Num = 1;
 
-let number=[3,1,6,8];
-let Max = Math.max(...number)
-let Num=1
+  let output = [];
 
-let output=[]
-
-for(i=Num;i<=Max;i++){
-   if(!number.includes(i)){
-    output.push(i)
-   }
-
-}
-console.log(output);
-
-
-
-
-
-
-
-
-
+  for (i = Num; i <= Max; i++) {
+    if (!number.includes(i)) {
+      output.push(i);
+    }
+  }
+  console.log(output);
 }
 
-missingNumber()
+missingNumber();
