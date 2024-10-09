d9b1f75ce07b4f1d681c481b7214504aed12485b
Aneesh165 <aneeshmp165@gmail.com>
2024-10-09 17:48:39 +0530

    9-10-24

--- CRUD/vite-project/src/Users.jsx
+++ CRUD/vite-project/src/Users.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const Users = () => {
-  return (
-    <div>Users</div>
-  )
-}
+  const [user, setUser] = useState([
+    {
+      Name: "Aneesh",
+      Email: "aneesh@gmail.com",
+      Age: "23",
+    },{
+        Name: "Rahman",
+      Email: "rahman@gmail.com",
+      Age: "21",
+    },{
+        Name: "AR",
+      Email: "ar165@gmail.com",
+      Age: "21",
+    }
+  ]);
 
-export default Users
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Age</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {user.map((user) => (
+            <tr>
+              <td>{user.Name}</td>
+              <td>{user.Email}</td>
+              <td>{user.Age}</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Users;
